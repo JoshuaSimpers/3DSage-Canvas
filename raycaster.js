e63fabdf88ebbe4051711c98d58d6eef4a3e3509
JoshuaSimpers <simpersjoshua@gmail.com>
2025-01-19 23:25:49 -0600

    added code that draws a yellow square at the position (300, 300) that represents the player

--- raycaster.js
+++ raycaster.js
@@ -1,2 +1,23 @@
 const canvas = document.getElementById("raycastCanvas");
 const context = canvas.getContext("2d");
+
+let px;
+let py;
+
+function drawPlayer()
+{
+    context.beginPath();
+    context.rect(px, py, 8, 8);
+    context.fillStyle = "yellow";
+    context.fill();
+    context.closePath();
+}
+
+function drawEverything()
+{
+    px = 300;
+    py = 300;
+    drawPlayer();
+}
+
+drawEverything();
